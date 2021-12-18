9b88ae20192c32de8081e193e4b68cf2f400a0c8
Dmitry <95961529+sw793@users.noreply.github.com>
2021-12-18 08:56:50 +0000

    re: 27856

---
+++ Sorted/25/25-8.cpp
@@ -0,0 +1,25 @@
+//https://inf-ege.sdamgia.ru/problem?id=27856
+
+// [95632; 95650], числа, имеющие ровно шесть различных нечётных натуральных делителей
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){	
+	for (int i = 95632; i < 95650 + 1; i++){
+		int arr[7];
+		int count = 0;
+		for (int j = 1; j < i + 1; j++){
+			if (((i % j) == 0) && ((j % 2) != 0)){
+				if (count > 6)
+					break;
+				arr[count] = j;
+				count++; 
+			}
+		}
+		if (count == 6){
+			for (int i = 0; i < 6; i++)
+				cout << arr[i] << " ";
+			cout << endl;
+		}
+	}
+}
