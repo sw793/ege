40ad586297c05434e8e912f26393a20a1f0131a5
Dmitry <dima@cryptolab.net>
2022-02-06 19:53:17 +0000

    13

--- Sorted/25/25-013.cpp
+++ Sorted/25/25-013.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 int main(){
 	int iteration = 0; //счетчик находок подходящего числа
+	
 	//перебор чисел
 	for (int num = 550000; num < 555000; num++){
 		if (iteration == 5) //если нашлось пять подходящих чисел, то дальше не работаем
 			break;
 		int sum = 0;
+		
 		//поиск делителей
 		for (int del = 2; del < num; del++){
 			if (num % del == 0){
 				int flag = 0; 
+				
 				//проверка на простое число
 				for (int i = 2; i < del; i++){
     				if (del % i == 0)
